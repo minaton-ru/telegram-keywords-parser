032bc7937c698977b5e60335ce0be03f4f290bbb
minaton-ru <minaton@yandex.ru>
2023-03-25 22:34:52 +0300

    переход на pyrogram

--- telegram-keywords-parser.py
+++ telegram-keywords-parser.py
@@ -1,12 +1,6 @@
 import configparser
-import messages
-
-from telethon.sync import TelegramClient
-from telethon import connection
-from telethon.sessions import StringSession
-
-# для корректного переноса времени сообщений в json
-from datetime import date, datetime
+# import messages
+from pyrogram import Client
 
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -14,16 +8,17 @@ config.read("config.ini")
 api_id   = config['Telegram']['API_ID']
 api_hash = config['Telegram']['API_HASH']
 username = config['Telegram']['USERNAME']
-session_string = config['Telegram']['SESSION_STRING']
+
+chat = 'your telegram chat name'
+limit = 4
+query='your text to search'
+
+app = Client(name=username, api_id=api_id, api_hash=api_hash)
 
 async def main():
-	url = input("Введите ссылку на канал или чат: ")
-	channel = await client.get_entity(url)
-	await messages.dump_all_messages(channel, client)
+	async with app:
+		async for message in app.search_messages(chat, query, limit):
+			print(message.text)
+			print('_______')
 
-with TelegramClient(StringSession(session_string), api_id, api_hash) as client:
-	# session_string = StringSession.save(client.session)
-	# print(session_string)
-	# with open('session.ini', 'w') as f:
-	# 	f.write(session_string)
-	client.loop.run_until_complete(main())
+app.run(main())
