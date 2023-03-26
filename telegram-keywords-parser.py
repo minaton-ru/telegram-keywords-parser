0998fa2b1568ac796bacece56f736ca7a619e00b
minaton-ru <minaton@yandex.ru>
2023-03-26 18:48:34 +0300

    raw messages search error

--- telegram-keywords-parser.py
+++ telegram-keywords-parser.py
@@ -1,6 +1,10 @@
+import datetime
 import configparser
+from dateutil.relativedelta import relativedelta
 # import messages
 from pyrogram import Client
+from pyrogram import enums
+from pyrogram.raw.functions import messages
 
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -9,15 +13,31 @@ api_id   = config['Telegram']['API_ID']
 api_hash = config['Telegram']['API_HASH']
 username = config['Telegram']['USERNAME']
 
-chat = 'your telegram chat name'
-limit = 4
-query='your text to search'
+start_date = datetime.datetime.now() - relativedelta(days=1)
+chat = 'javascript_jobs_feed'
+mylimit = 4
+query='Григорий'
 
 app = Client(name=username, api_id=api_id, api_hash=api_hash)
 
 async def main():
 	async with app:
-		async for message in app.search_messages(chat, query, limit):
+		search_result = await app.invoke(
+			messages.Search(
+				peer=chat, 
+				q=query, 
+				limit=mylimit, 
+				min_date=start_date, 
+				max_date=0, 
+				filter=enums.MessagesFilter.EMPTY,
+				offset_id=0,
+				add_offset=0,
+				min_id=0,
+				max_id=0,
+				hash=0
+			)
+		)
+		for message in search_result:
 			print(message.text)
 			print('_______')
 
