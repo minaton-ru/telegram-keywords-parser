2bf2bf17dd70e4069333917a7b37752abe074b53
minaton-ru <minaton@yandex.ru>
2023-04-01 16:32:09 +0300

    keywords list from file

--- telegram-keywords-parser.py
+++ telegram-keywords-parser.py
@@ -12,19 +12,21 @@ api_hash = config['Telegram']['API_HASH']
 username = config['Telegram']['USERNAME']
 
 start_date = datetime.datetime.now() - relativedelta(days=1) # Формируем дату на нужное количество дней назад
-chat = '' # Название нужного телеграм-канала
+chat = 'javascript_jobs_feed' # Название нужного телеграм-канала
 limit = 10 # Максимальное количество сообщений, в которых будет поиск
-keywords_list=['', ''] # список ключевых слов
+
+with open('keywords.txt', 'r') as file:
+	keywords_list=file.read().lower().splitlines() # список ключевых слов прочитали построчно из файла, привели к нижнему регистру
 
 app = Client(name=username, api_id=api_id, api_hash=api_hash)
 
 async def main():
 	async with app:		
 		async for message in app.get_chat_history(chat, limit=limit):
-			if message.date > start_date: # Только сообщения, которые появились после нужной даты
+			if message.date >= start_date: # Только сообщения, которые появились после нужной даты
 				message_text = message.text
 				message_words_list = re.sub("[^a-zа-яёїієґ0-9_-]", " ",  message_text.lower()).split() # Очищаем текст сообщения от символов и пунктуации, разбиваем на слова
 				if any(word in keywords_list for word in message_words_list): # Проверяем вхождение каждого из ключевых слов в тексте сообщения
-					print(f'==== {message.date} {message.id}')
+					print(f'==== {message.date} https://t.me/{chat}/{message.id}')
 
 app.run(main())
