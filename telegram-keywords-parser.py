911b8f0873398cb6e62bf9a299928ad7f5591604
minaton-ru <minaton@yandex.ru>
2023-04-02 20:47:43 +0300

    chat list from file

--- telegram-keywords-parser.py
+++ telegram-keywords-parser.py
@@ -1,4 +1,5 @@
 import datetime
+import asyncio
 import re
 import configparser
 from dateutil.relativedelta import relativedelta
@@ -11,12 +12,14 @@ api_id   = config['Telegram']['API_ID']
 api_hash = config['Telegram']['API_HASH']
 username = config['Telegram']['USERNAME']
 
-start_date = datetime.datetime.now() - relativedelta(days=3) # Формируем дату на нужное количество дней назад
-chat_list = ['', '', ''] # Список нужных телеграм-каналов
-limit = 10 # Максимальное количество сообщений, в которых будет поиск
+start_date = datetime.datetime.now() - relativedelta(days=2) # Формируем дату на нужное количество дней назад
+limit = 10 # Максимальное количество сообщений из каждого телеграм-канала, в которых будет поиск
 
 with open('keywords.txt', 'r') as file:
 	keywords_list=file.read().lower().splitlines() # список ключевых слов прочитали построчно из файла, привели к нижнему регистру
+
+with open('chats.txt', 'r') as file:
+	chat_list=file.read().splitlines() # список ключевых слов прочитали построчно из файла, привели к нижнему регистру
 
 app = Client(name=username, api_id=api_id, api_hash=api_hash)
 
@@ -35,5 +38,6 @@ async def main():
 					message_words_list = re.sub("[^a-zа-яёїієґ0-9_-]", " ",  message_text.lower()).split() # Очищаем текст сообщения от символов и пунктуации, разбиваем на слова
 					if any(word in keywords_list for word in message_words_list): # Проверяем вхождение каждого из ключевых слов в тексте сообщения
 						print(f'==== {message.date} https://t.me/{chat}/{message.id}')
+					await asyncio.sleep(1)
 
 app.run(main())
